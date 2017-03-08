2ddaa2c4e06bca2d3aaa60ed96bfdcef2c99a19e
Erica Nafziger <garbererica@gmail.com>
2017-03-08 09:14:44 -0800

    add mobile menu with subdropdowns to documentaion - update nav.scss so fly dropdowns have a pointer cursor on hover

--- js/tuxedo-documentation.js
+++ js/tuxedo-documentation.js
@@ -3,23 +3,14 @@ $(document).ready(function() {
   $('#mobile-nav-example .hamburger-2').click(function() {
     $('#mobile-nav-example .flyout').show();
     $('#mobile-nav-example').css({'z-index': '1'});
-    $('#mobile-nav-example .flyout').css({'height': '500px'});
+    $('#mobile-nav-example .flyout').css({'height': '600px'});
   });
   $('#mobile-nav-drop-example .hamburger-2').click(function() {
     $('#mobile-nav-drop-example .flyout').show();
     $('#mobile-nav-drop-example').css({'z-index': '1'});
-    $('#mobile-nav-drop-example .flyout').css({'height': '500px'});
+    $('#mobile-nav-drop-example .flyout').css({'height': '600px'});
   });
-  // $('.close').click(function() {
-  //   $('.flyout').hide();
-  //   $('body').css({'overflow': 'auto'});
-  // });
-  // $('.fly-dropdown').click(function() {
-  //   $(this).children('.dropdown-menu').fadeToggle();
-  // });
-  // $('.sub-fly-dropdown').hover(function() {
-  //   $(this).children('.sub-dropdown-menu').fadeToggle();
-  // });
+
 
 
 });
