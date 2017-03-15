7eb6cb65c2e4b03638d02e6a9a491528941c011c
Erica Nafziger <garbererica@gmail.com>
2017-03-14 17:13:53 -0700

    add hover functionality to d3 pie chart

--- js/d3.js
+++ js/d3.js
@@ -1,6 +1,6 @@
 //============Pie Chart=========//
 var labelColor = '#f4f4f4',
-  donutChart = true; //change to true for donutChart or false for pie chart
+  donutChart = false; //change to true for donutChart or false for pie chart
 
 //margin and radius
 var margin = {top: 20, right: 20, bottom: 20, left: 20},
@@ -19,10 +19,9 @@ var arc = d3.arc()
     }
   })
 
-
 var labelArc = d3.arc()
-  .outerRadius(radius - 50)
-  .innerRadius(radius - 50)
+  .outerRadius(radius - 60)
+  .innerRadius(radius - 60)
 
 //pie generator
 var pie = d3.pie()
@@ -36,18 +35,11 @@ var svg = d3.selectAll('.pieChart').append('svg')
   .append('g')
   .attr('transform', 'translate(' + width/2 + ', ' + height/2 + ')');
 
-  //import data
-  // d3.csv('data.csv', function(error, data) {
-  //   if (error) throw error;
-  //   //parse data
-  //   data.forEach(function(d) {
-  //     d.count = +d.count;
-  //     d.item = d.item;
-  //     d.color = d.color;
-  //   });
 
-  d3.csv("data.csv", function(error, data) {
-    console.log(data);
+
+
+  //import data
+  d3.json("data.json", function(error, data) { //change json to csv to change data type
     if (error) throw error;
     //parse data
     data.forEach(function(d) {
@@ -56,34 +48,60 @@ var svg = d3.selectAll('.pieChart').append('svg')
       d.color = d.color;
     });
 
-  // })
 
     //append g elements (arc)
     var g = svg.selectAll('.arc')
       .data(pie(data))
       .enter().append('g')
+      // .on("mouseover", function() {
+      //     d3.select(this).classed("hover", true);
+      // })
+      // .on("mouseout", function() {
+      //     d3.select(this).classed("hover", false);
+      // })
       .attr('class', 'arc');
 
     //append path of the arc
     g.append('path')
       .attr('d', arc)
       .style('fill', function(d) { return d.data.color})
+      .style('opacity', 0.8)
+      .on("mouseover", function() { //add hover functionality
+        d3.select(this).transition()
+        .style("opacity", 1)
+        .style("cursor", 'pointer')
+        d3.select(this.parentNode).selectAll("text")
+        .transition()
+        .duration(200)
+        .style('opacity', 1)
+
+      })
+      .on("mouseout", function() { //take away hover changes
+        d3.select(this).transition()
+        .style("opacity", 0.8)
+        d3.select(this.parentNode).selectAll("text")
+        .transition()
+        .duration(200)
+        .style('opacity', 0)
+      })
       .transition()
       .ease(d3.easeLinear)
       .duration(1500)
       .attrTween('d', pieTween)
 
+
     //append text (labels)
     g.append('text')
       .style('fill', labelColor)
+      .style('opacity', 0)
       .transition()
       .delay(1500)
       .ease(d3.easeLinear)
-      .duration(800)
-      .attrTween('d', pieTween)
+      .duration(400)
+      // .attrTween('d', pieTween)
       .attr('transform', function(d) { return 'translate(' + labelArc.centroid(d) + ')';})
       .attr('dy', '.35em')
-      .text(function(d) {return d.data.species})
+      .text(function(d) {return d.data.species + " (" + d.data.weight + " lbs)"})
 
   });
 
